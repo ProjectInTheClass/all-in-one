bcfb2314730668b0ac752b3619c83ec93942e3c5
Dookong <dookong727@gmail.com>
2022-06-10 00:48:36 +0900

    news fix

--- all-in-one/features/calendar/data/EconomyNews.swift
+++ all-in-one/features/calendar/data/EconomyNews.swift
@@ -15,50 +15,6 @@ struct EconomyNews: Codable {
 
 // MARK: - Article
 struct Article: Codable {
-    let source: Source
-    let author: String?
-    let title, articleDescription: String
+    let title: String
     let url: String
-    let urlToImage: String
-    let publishedAt: Date
-    let content: String?
-
-    enum CodingKeys: String, CodingKey {
-        case source, author, title
-        case articleDescription = "description"
-        case url, urlToImage, publishedAt, content
-    }
-}
-
-// MARK: - Source
-struct Source: Codable {
-    let id: JSONNullNews?
-    let name: String
-}
-
-// MARK: - Encode/decode helpers
-
-class JSONNullNews: Codable, Hashable {
-
-    public static func == (lhs: JSONNullNews, rhs: JSONNullNews) -> Bool {
-        return true
-    }
-    
-    func hash(into hasher: inout Hasher) {
-        return
-    }
-
-    public init() {}
-
-    public required init(from decoder: Decoder) throws {
-        let container = try decoder.singleValueContainer()
-        if !container.decodeNil() {
-            throw DecodingError.typeMismatch(JSONNullNews.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Wrong type for JSONNull"))
-        }
-    }
-
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.singleValueContainer()
-        try container.encodeNil()
-    }
 }
