32c4f1c86088be4a6fcda069739d41019772617e
2016025087 <2016025087@hanyang.ac.kr>
2022-06-12 00:03:22 +0900

    edit UIs

--- all-in-one/features/main/data/StarredCell.swift
+++ all-in-one/features/main/data/StarredCell.swift
@@ -22,9 +22,15 @@ class StarredCell: UITableViewCell {
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
     }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top:3.0, left: 0.0, bottom: 5.0, right: 0.0))
+    }
     
     @IBAction func saveStatus(_ sender: UIButton) {
         sender.isSelected.toggle()
         UserDefaults.standard.set(sender.isSelected, forKey:cellTicker)
     }
+    
 }
